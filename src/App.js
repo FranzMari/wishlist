796c677c5e9782475304304d55d280bf5081bd97
Francesco <francesco.marinucci@linux.it>
2022-05-15 20:46:51 +0200

    spostate liste da json a stato

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import MenuAppBar from "./components/MenuAppBar";
 import AppDrawer from "./components/AppDrawer";
-import Lists from "./components/Lists"
+import Lists from "./components/Lists";
 
 function App() {
   const [pageInfo, setPageInfo] = React.useState({
@@ -11,21 +11,22 @@ function App() {
   });
 
   const setDrawerOpen = () => {
-    setPageInfo(prevState =>  {
-        return {
-            ...prevState,
-            isDrawerOpen: !prevState.isDrawerOpen
-        }
+    setPageInfo((prevState) => {
+      return {
+        ...prevState,
+        isDrawerOpen: !prevState.isDrawerOpen,
+      };
     });
-}
+  };
 
   return (
     <div className="App">
-      <MenuAppBar title={pageInfo.pageTitle} handleClick={setDrawerOpen}/>
-      <AppDrawer 
+      <MenuAppBar title={pageInfo.pageTitle} handleClick={setDrawerOpen} />
+      <AppDrawer
         isOpen={pageInfo.isDrawerOpen}
         handleOnClose={setDrawerOpen}
-        handleOnOpen={setDrawerOpen} />
+        handleOnOpen={setDrawerOpen}
+      />
       <Lists />
     </div>
   );
--- src/components/Lists.js
+++ src/components/Lists.js
@@ -1,38 +1,64 @@
 import * as React from "react";
-import { List, ListItem, ListItemText, IconButton, Stack, Box, Fab } from "@mui/material";
+import {
+  List,
+  ListItem,
+  ListItemText,
+  IconButton,
+  Stack,
+  Box,
+  Fab,
+} from "@mui/material";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
-import AddIcon from "@mui/icons-material/Add"
-import lists from "../lists.json";
+import AddIcon from "@mui/icons-material/Add";
 
 export default function Lists() {
-    const fabStyle = {
-        position: 'absolute',
-        bottom: 16,
-        right: 16,
-    }
+  const [wishlists, setWishlists] = React.useState([]);
+
+  const newWishlist = () => {
+    setWishlists((prevState) => {
+      return {
+        ...prevState,
+        isDrawerOpen: !prevState.isDrawerOpen,
+      };
+    });
+  };
+
+  const fabStyle = {
+    position: "absolute",
+    bottom: 16,
+    right: 16,
+  };
   return (
     <Box>
-        <List>
-        {lists.Lists.map((list) => (
-            <ListItem button key={lists.Lists.indexOf(list)}
+      <List>
+        {wishlists.map((list) => (
+          <ListItem
+            button
+            key={wishlists.indexOf(list)}
             secondaryAction={
-                <Stack direction="row" spacing={2}>
-                    <IconButton edge="end" aria-label="edit">
-                        <EditIcon />
-                    </IconButton>
-                    <IconButton edge="end" aria-label="delete">
-                        <DeleteIcon />
-                    </IconButton>
-                </Stack>
-            }>
+              <Stack direction="row" spacing={2}>
+                <IconButton edge="end" aria-label="edit">
+                  <EditIcon />
+                </IconButton>
+                <IconButton edge="end" aria-label="delete">
+                  <DeleteIcon />
+                </IconButton>
+              </Stack>
+            }
+          >
             <ListItemText primary={list.name} />
-            </ListItem>
+          </ListItem>
         ))}
-        </List>
-        <Fab sx={fabStyle} color="primary" aria-label="add">
-            <AddIcon />
-        </Fab>
+      </List>
+      <Fab
+        sx={fabStyle}
+        color="primary"
+        aria-label="add"
+        handleClick={newWishlist}
+      >
+        <AddIcon />
+      </Fab>
     </Box>
   );
 }
